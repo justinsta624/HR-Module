785e70839b09ac49bf08f78ddcc2255452948a3f
Dan Nadler <dan.nadlerp@gmail.com>
2024-03-13 19:26:21 -0400

    updated index.js model associations

--- server/models/index.js
+++ server/models/index.js
@@ -1,30 +1,25 @@
-const User = require('./User');
-const Department = require('./Department');
-const Role = require('./Role');
-// const Employee = require('./Employee');
+const User = require("./User");
+const Department = require("./Department");
+const Role = require("./Role");
+const Employee = require("./Employee");
 
-User.hasMany(Department, {
-  foreignKey: 'user_id',
+Department.hasMany(User, {
+  foreignKey: "department_id",
 });
 
-Department.belongsTo(User, {
-  foreignKey: 'user_id',
+User.belongsTo(Department, {
+  foreignKey: "department_id",
 });
-
-Department.hasMany(Role, {
-  foreignKey: 'department_id',
+Employee.belongsTo(Role, {
+  foreignKey: "role_id",
 });
 
 Role.belongsTo(Department, {
-  foreignKey: 'department_id',
+  foreignKey: "department_id",
 });
 
-// Department.hasMany(Role, {
-//   foreignKey: 'department_id',
-// });
+Department.hasMany(Role, {
+  foreignKey: "department_id",
+});
 
-// Role.hasMany(Employee, {
-//   foreignKey: 'role_id',
-// });
-
-module.exports = { User, Department, Role/*, Employee*/ };
+module.exports = { User, Department, Role, Employee };
