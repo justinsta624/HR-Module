849f16d95d705d4c1f2716d1682fed9ab6aca887
Ozdal Dogru <ozdaldogru@gmail.com>
2024-03-14 02:57:14 -0400

    client/src/pages folder created and some files transfered into it.EmployeeList.jsx file is all updated

---
+++ client/src/pages/EmployeeAdd.jsx
@@ -0,0 +1,132 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AddEmployee = () => {
+  const [employee, setEmployee] = useState({
+    fist_name:'',
+    last_name:'',
+    email:'',
+    salary:'',
+    manager:'',
+    role:'',
+  });
+  const [role, setRole] = useState([]);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    axios
+      .get("api/roles")
+      .then((result) => {
+        if (result.data.Status) {
+            setRole(result.data.Result);
+        } else {
+          alert(result.data.Error);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData();
+    formData.append('first_name', employee.first_name);
+    formData.append('last_name', employee.last_name);
+    formData.append('email', employee.email);
+    formData.append('salary', employee.salary);
+    formData.append('manager', employee.is_manager);
+    formData.append('role', employee.role);
+
+    axios.post('/api/employees/', formData)
+    .then(result => {
+        if(result.data.Status) {
+            navigate('/api/employees')
+        } else {
+            alert(result.data.Error)
+        }
+    })
+    .catch(err => console.log(err))
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center mt-3">
+      <div className="p-3 rounded w-50 border">
+        <h3 className="text-center">Add Employee</h3>
+        <form className="row g-1" onSubmit={handleSubmit}>
+          
+          <div className="col-12">
+            <label htmlFor="inputName" className="form-label"> First Name</label>
+            <input
+              type="text"
+              className="form-control rounded-0"
+              id="inputName"
+              placeholder="Enter Name"
+              onChange={(e) =>
+                setEmployee({ ...employee, name: e.target.value })
+              }
+            />
+          </div>
+
+          <div className="col-12">
+            <label htmlFor="inputName" className="form-label">Last Name</label>
+            <input
+              type="text"
+              className="form-control rounded-0"
+              id="inputName"
+              placeholder="Enter Name"
+              onChange={(e) =>
+                setEmployee({ ...employee, name: e.target.value })
+              }
+            />
+          </div>
+
+
+
+          <div className="col-12">
+            <label htmlFor="inputEmail4" className="form-label">Email</label>
+            <input
+              type="email"
+              className="form-control rounded-0"
+              id="inputEmail4"
+              placeholder="Enter Email"
+              autoComplete="off"
+              onChange={(e) =>
+                setEmployee({ ...employee, email: e.target.value })
+              }
+            />
+          </div>
+
+          <div className="col-12">
+
+            <label htmlFor="inputSalary" className="form-label">Salary</label>
+            <input
+              type="text"
+              className="form-control rounded-0"
+              id="inputSalary"
+              placeholder="Enter Salary"
+              autoComplete="off"
+              onChange={(e) =>
+                setEmployee({ ...employee, salary: e.target.value })
+              }
+            />
+          </div>
+
+
+
+
+
+
+     
+
+
+
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary w-100">Add Employee</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddEmployee;
