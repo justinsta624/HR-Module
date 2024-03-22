d04aa212b1e3b3ea38b33568825d868ff1505a48
Austin Graham <austiingraham@gmail.com>
2024-03-21 21:31:19 -0400

    updated navbar responsiveness

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,52 +3,63 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
 import SignUpForm from './SignupForm';
 import LoginForm from './LoginForm';
-
 import Auth from '../utils/auth';
+import '../styles/Navbar.css';
 
+// Define the AppNavbar component to display the navigation bar
 const AppNavbar = () => {
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false); // State to track modal visibility
   const [loggedIn, setLoggedIn] = useState(false); // State to track login status
 
   useEffect(() => {
     // Update login status when component mounts
-    setLoggedIn(Auth.loggedIn());
+    setLoggedIn(Auth.loggedIn()); // Check if user is logged in
   }, []);
 
   const handleLogin = () => {
     // After successful login, update login status
-    setLoggedIn(true);
-    setShowModal(false);
+    setLoggedIn(true); // Set the login status to true
+    setShowModal(false); // Close the modal
   };
 
   const handleLogout = () => {
     // After logout, update login status
-    Auth.logout();
-    setLoggedIn(false);
+    Auth.logout(); // Clear the token from local storage
+    setLoggedIn(false); // Set the login status to false
+  };
+
+  // Function to close the navbar when a link is clicked
+  const handleNavLinkClick = () => {
+    if (window.innerWidth < 768) { // Close navbar if screen size is less than to 768px (Bootstrap md breakpoint)
+      const navbarToggler = document.querySelector('.navbar-toggler'); // Select the navbar toggler button
+      if (navbarToggler) { // If the navbar toggler button is found
+        navbarToggler.click(); // Simulate click on navbar toggler button
+      }
+    }
   };
 
   return (
     <>
-      <Navbar bg='dark' variant='dark' expand='lg'>
+      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container fluid>
-          <Navbar.Brand as={Link} to='/'>
-            💻 Employee Management System: EMS 💼
+          <Navbar.Brand as={Link} to='/' onClick={handleNavLinkClick}>
+            Employee Management System
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
-            <Nav className='ml-auto d-flex'>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse className='justify-content-end' id='responsive-navbar-nav'>
+            <Nav className='ml-auto text-end'>
               {loggedIn && (
                 <>
-                  <Nav.Link as={Link} to='/departments'>
+                  <Nav.Link as={Link} to='/departments' onClick={handleNavLinkClick}>
                     Departments
                   </Nav.Link>
-                  <Nav.Link as={Link} to='/managers'>
+                  <Nav.Link as={Link} to='/managers' onClick={handleNavLinkClick}>
                     Managers
                   </Nav.Link>
-                  <Nav.Link as={Link} to='/employees'>
+                  <Nav.Link as={Link} to='/employees' onClick={handleNavLinkClick}>
                     Employees
                   </Nav.Link>
-                  <Nav.Link as={Link} to='/roles'>
+                  <Nav.Link as={Link} to='/roles' onClick={handleNavLinkClick}>
                     Roles
                   </Nav.Link>
                   <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
@@ -97,86 +108,3 @@ const AppNavbar = () => {
 };
 
 export default AppNavbar;
-
-// import { useState } from 'react'; // Importing the useState hook from React
-// import { Link } from 'react-router-dom'; // Importing the Link component from react-router-dom
-// import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap'; // Importing various components from react-bootstrap
-// import SignUpForm from './SignupForm'; // Importing the SignUpForm component from './SignupForm'
-// import LoginForm from './LoginForm'; // Importing the LoginForm component from './LoginForm'
-
-// import Auth from '../utils/auth'; // Importing the Auth module from '../utils/auth'
-
-// const AppNavbar = () => { // Defining a functional component called AppNavbar
-//   // set modal display state
-//   const [showModal, setShowModal] = useState(false); // Declaring a state variable 'showModal' and a function 'setShowModal' to control the visibility of the modal
-
-//   return (
-//     <>
-//       <Navbar bg='dark' variant='dark' expand='lg'>
-//         <Container fluid>
-//           <Navbar.Brand as={Link} to='/'>
-//           💻 Employee Management System: HRRIS 💼
-//           </Navbar.Brand>
-//           <Navbar.Toggle aria-controls='navbar' />
-//           <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
-//             <Nav className='ml-auto d-flex'>
-//               <Nav.Link as={Link} to='/departments'>
-//                 Departments
-//               </Nav.Link>
-//               <Nav.Link as={Link} to='/managers'>
-//                 Managers
-//               </Nav.Link>
-//               <Nav.Link as={Link} to='/employees'>
-//                 Employees
-//               </Nav.Link>
-//               <Nav.Link as={Link} to='/roles'>
-//                 Roles
-//               </Nav.Link>
-//               {Auth.loggedIn() ? (
-//                 <>
-//                   <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-//                 </>
-//               ) : (
-//                 <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-//               )}
-//             </Nav>
-//           </Navbar.Collapse>
-//         </Container>
-//       </Navbar>
-//       {/* set modal data up */}
-//       <Modal
-//         size='lg'
-//         show={showModal}
-//         onHide={() => setShowModal(false)}
-//         aria-labelledby='signup-modal'>
-//         {/* tab container to do either signup or login component */}
-//         <Tab.Container defaultActiveKey='login'>
-//           <Modal.Header closeButton>
-//             <Modal.Title id='signup-modal'>
-//               <Nav variant='pills'>
-//                 <Nav.Item>
-//                   <Nav.Link eventKey='login'>Login</Nav.Link>
-//                 </Nav.Item>
-//                 <Nav.Item>
-//                   <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-//                 </Nav.Item>
-//               </Nav>
-//             </Modal.Title>
-//           </Modal.Header>
-//           <Modal.Body>
-//             <Tab.Content>
-//               <Tab.Pane eventKey='login'>
-//                 <LoginForm handleModalClose={() => setShowModal(false)} />
-//               </Tab.Pane>
-//               <Tab.Pane eventKey='signup'>
-//                 <SignUpForm handleModalClose={() => setShowModal(false)} />
-//               </Tab.Pane>
-//             </Tab.Content>
-//           </Modal.Body>
-//         </Tab.Container>
-//       </Modal>
-//     </>
-//   );
-// };
-
-// export default AppNavbar; // Exporting the AppNavbar component
